77d08dbe981fc93ca6094c8285fa2babe9898141
HJ Cross <github@chimpjuice.com>
2016-12-08 22:55:38 -0500

    update server to demo swipe

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ app.get('/', function (req, res) {
 })
 
 var responsivePath = path.join(__dirname, '/html/Skeleton-2.0.4');
-console.log(responsivePath);
+//console.log(responsivePath);
 app.use('/responsive', express.static(responsivePath));
+
+var swipePath = path.join(__dirname, '/html/skeleton-swipe');
+//console.log(responsivePath);
+app.use('/swipe', express.static(swipePath));
+
 
 app.listen(appPort, function () {
   console.log('Example app listening on port ' + appPort + '!')
 })
-
