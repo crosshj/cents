ffdb6fde1cd3e9ff941ee68333ce07a67f4faf31
crosshj <github@chimpjuice.com>
2017-03-18 23:53:09 -0400

    early sketches for service

---
+++ service/examples/usaa.js
@@ -0,0 +1,50 @@
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: false, frame: false });
+var path = require('path');
+import { usaa } as getPrivateInfo from './getPrivateInfo';
+
+nightmare
+  .goto('https://mobile.usaa.com/inet/ent_logon/Logon?acf=1')
+  .click('#logOnButton a')
+  .type('input#input_onlineid', getPrivateInfo().username())
+  .type('input#input_password', getPrivateInfo().password())
+  .click('input[type="submit"]')
+  .wait('input#pinTextField')
+  .type('input#pinTextField', getPrivateInfo().pin())
+  .click('button[type="submit"]')
+  .wait('label[for="securityQuestionTextField"]')
+  .type('#securityQuestionTextField', getPrivateInfo().answer(
+    [...document.querySelector('label[for="securityQuestionTextField"]').innerHTML]
+  ))
+  .click('button[type="submit"]')
+  .wait('#menu')
+  .screenshot(path.join(__dirname, 'usaa.png'))
+  .end()
+    .then(function (result) {
+      console.log(result)
+    })
+    .catch(function (error) {
+      console.error('Error:', error);
+    });
+
+
+    // nightmare
+    //   .goto('https://mobile.usaa.com/inet/ent_logon/Logon?acf=1')
+    //   .click('input#input_onlineid')
+    //   .click('div#main-ctr > div.section-ctr:nth-child(4)')
+    //   .click('input#input_password')
+    //   .click('div#main-ctr > div.section-ctr:nth-child(4)')
+    //   .click('div#main-ctr > div.section-ctr:nth-child(4) > div.button-ctr:nth-child(1) > div.yui3-g.padded:nth-child(1) > div.yui3-u-1:nth-child(1) > input.main-button:nth-child(1)')
+    //   .click('input#pinTextField')
+    //   .click('button#id4')
+    //   .click('input#securityQuestionTextField')
+    //   .click('div#flowWrapper')
+    //   .click('button#id6')
+    //   .click('div#id3 > ul.acct-group-list:nth-child(1) > li.acct-group-row:nth-child(1) > a.usaa-link.acct-info.clearfix:nth-child(1) > span.link-liner:nth-child(1) > span.acct-name:nth-child(1)')
+    //   .end()
+    //     .then(function (result) {
+    //       console.log(result)
+    //     })
+    //     .catch(function (error) {
+    //       console.error('Error:', error);
+    //     });
