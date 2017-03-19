b1057222e9aee030adfb35315881262375137652
crosshj <github@chimpjuice.com>
2017-03-18 23:53:09 -0400

    early sketches for service

---
+++ service/examples/example1.js
@@ -0,0 +1,20 @@
+var Nightmare = require('nightmare');
+var nightmare = Nightmare({ show: false, frame: false });
+var path = require('path');
+
+nightmare
+  .goto('https://duckduckgo.com')
+  .type('#search_form_input_homepage', 'github nightmare')
+  .click('#search_button_homepage')
+  .wait('#zero_click_wrapper .c-info__title a')
+  .evaluate(function () {
+    return document.querySelector('#zero_click_wrapper .c-info__title a').href;
+  })
+  .screenshot(path.join(__dirname, 'screenshot.png'))
+  .end()
+  .then(function (result) {
+    console.log(result);
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
