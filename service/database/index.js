65c6f57d22beac38d070b8b3ee5be3740fe58543
crosshj <github@chimpjuice.com>
2017-03-21 13:27:26 -0400

    create basic database and test

---
+++ service/database/index.js
@@ -0,0 +1,52 @@
+var Db = require('tingodb')().Db;
+var path = require('path');
+
+var db;
+var collection;
+
+const defaultCallback = operation => {
+  return (err, result) => {
+    if (err){
+      return console.log('ERROR - default DB ' + operation.toUpperCase() + ' callback:\n', JSON.stringify(err, null, ' '))
+    }
+    console.log('SUCCESS - default DB ' + operation.toUpperCase() + ' callback');
+    result && console.log('Result:\n', JSON.stringify(result, null, ' '))
+  };
+};
+
+const init = ({
+  collectionName = 'default',
+  dbPath = path.join(__dirname, '/data'),
+  callback = () => {}
+} = {}) => {
+  db = new Db(dbPath, {});
+  collection = db.collection(collectionName);
+  callback();
+};
+
+const create = ({docs, callback = defaultCallback('create')} = {}) => {
+  collection.insert(docs, {w:1}, callback);
+};
+
+const read = ({query, callback = defaultCallback('read')} = {}) => {
+  collection.find(query).toArray(callback);
+};
+
+const update = ({query, update, callback = defaultCallback('update')} = {}) => {
+  collection.update(query, { $set: update }, err => {
+    if (err) return callback(err);
+    collection.findOne(query, callback);
+  });
+};
+
+const del = ({query, callback = defaultCallback('delete')} = {}) => {
+  collection.remove(query, {safe: true}, callback);
+};
+
+module.exports = {
+  init,
+  create,
+  read,
+  update,
+  del
+}
