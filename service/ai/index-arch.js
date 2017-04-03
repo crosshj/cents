cbed9882afb65a6b568181e4475546eca57deaec
crosshj <github@chimpjuice.com>
2017-04-02 23:21:46 -0400

    tweaking NN setup

--- service/ai/index-arch.js
+++ service/ai/index-arch.js
@@ -19,48 +19,33 @@
 const path = require('path');
 const revert = require('./revert');
 const trainingSet = require(path.join(__dirname, '../../logs/scrape.log.converted.json'));
-const scraped = clone(trainingSet);
-//const testSet = set.test;
-//console.log(trainingSet)
+
 const synaptic = require('synaptic');
-const Layer = synaptic.Layer;
-const Network = synaptic.Network;
+const Architect = synaptic.Architect;
 const Trainer = synaptic.Trainer;
 
-const inputLayer = new Layer(9);
-const hiddenLayer = new Layer(8);
-const outputLayer = new Layer(1);
+const scraped = clone(trainingSet);
 
-inputLayer.project(hiddenLayer);
-hiddenLayer.project(outputLayer);
+const myNetwork = new Architect.Perceptron(9,5,1);
 
-const myNetwork = new Network({
-    input: inputLayer,
-    hidden: [hiddenLayer],
-    output: outputLayer
-});
+//const myNetwork = new Architect.Perceptron(9,5,5,1);
+
+// var input = 9;
+// var pool = 20;
+// var output = 1;
+// var connections = 30;
+// var gates = 10;
+// const myNetwork = new Architect.Liquid(input, pool, output, connections, gates);
 
 const trainer = new Trainer(myNetwork);
 const trainingOptionsA = {
   rate: .1,
-  error: .000001,
+  error: .00001,
   shuffle: true,
   log: 1
 };
-// const trainingOptionsB = {
-//     rate: .2,
-//     iterations: 20,
-//     error: .1,
-//     shuffle: true,
-//     log: 1,
-//     cost: Trainer.cost.CROSS_ENTROPY
-// };
 trainer.train(trainingSet, trainingOptionsA);
 
-// does network predict inputs? should be true for all cases
-// trainingSet.forEach(item =>
-//   console.log(Math.round(myNetwork.activate(item.input)[0]) === item.output[0])
-// );
 
 function clone(item){
   return JSON.parse(JSON.stringify(item));
