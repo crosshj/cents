d0763c8fa2f1ae8a25343dab01b519e9516982b2
crosshj <github@chimpjuice.com>
2017-05-16 01:28:11 -0400

    fix a bunch of problems with fresh start of app

--- service/routes/index.js
+++ service/routes/index.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 
-module.exports = function(app) {
+module.exports = function(app, passport) {
   fs.readdirSync(__dirname).forEach(function(file) {
     if (file === "index.js" || file.substr(file.lastIndexOf('.') + 1) !== 'js')
         return;
     var name = file.substr(0, file.indexOf('.'));
-    require('./' + name)(app);
+    require('./' + name)(app, passport);
   });
 };
