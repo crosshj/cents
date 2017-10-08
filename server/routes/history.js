0fdc7916883077aeed3826b8e90cb59147aff346
HJ Cross <github@chimpjuice.com>
2017-10-07 23:19:17 -0400

    parse diffs logs and return useable diffs, npm watch

--- server/routes/history.js
+++ server/routes/history.js
@@ -3,6 +3,41 @@ const path = require('path');
 
 const DIFF_LOG_LOCATION = path.resolve(__dirname, '../../diffs.log');
 
+//(+date)delta/liabilities/changes/(+key)(?update)changes/(?amount)(+value)(+oldValue)update
+// -> { date, key, value, oldValue }
+function filterDiffs(lines, type, fields, account){
+  const diffs = lines
+    .filter(x => x.delta.length > 0)
+    .reduce((out, x) => {
+      x.delta
+        .filter(d => d.changes && d.changes.length > 0)
+        .filter(d => d.key === type)
+        .forEach(d => {
+            d.changes
+              .filter(lc => lc.changes && lc.changes.length > 0)
+              .filter(lc => !account || lc.key.toLowerCase() === account.toLowerCase())
+              .forEach(lc => {
+                lc.changes
+                  .filter(ic => fields.includes(ic.key))
+                  .filter(ic => 
+                    ic.oldValue && ic.value
+                    && Number(ic.oldValue) !== Number(ic.value))
+                  .forEach(ic => {
+                    out.push({
+                      date: x.date,
+                      account: lc.key,
+                      field: ic.key,
+                      value: ic.value,
+                      oldValue: ic.oldValue
+                    })
+                  })
+              })
+        })
+      return out;
+    }, []);
+  return diffs;
+}
+
 function getDiffs(req, res) {
   fs.readFile(DIFF_LOG_LOCATION, 'utf-8', (err, file) => {
     if (err) {
@@ -16,7 +51,10 @@ function getDiffs(req, res) {
         .map(item => JSON.parse(item));
 
       res.setHeader('content-type', 'text/plain');
-      res.send(lines);
+      const fields = ['amount', 'total_owed'];
+      const account = req.query.account;
+      const diffs = filterDiffs(lines, 'liabilities', fields, account);
+      res.send(diffs);
     } catch (e) {
       //console.log(`error with sending lines from ${DIFF_LOG_LOCATION}: ${e}`);
       res.send(e);
