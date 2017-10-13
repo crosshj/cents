72e079c1d4e154d4f41f99d23e6bbbbf214be418
HJ Cross <github@chimpjuice.com>
2017-10-12 22:54:28 -0400

    auto status functionality, refactor server/client
    move update account logic to server
    auto checkbox in UI
    auto-pending for auto accounts
    auto property for each account

--- client/js/accountData.js
+++ client/js/accountData.js
@@ -19,59 +19,19 @@ var unFormatMoney = function(money){
 function formatAccountData(data){
     window.MAIN_DATA = data;
 
-    var today = new Date();
-    var oneWeekAhead = new Date().setDate(today.getDate()+7);
-    MAIN_DATA.liabilities = MAIN_DATA.liabilities.map(function(item){
-      if (item.status.toLowerCase() === 'paid' && new Date(item.date) <= oneWeekAhead){
-        item.status = "due";
-      }
-      return item;
-    });
-
     MAIN_DATA.liabilities.getByName = title => getByName(MAIN_DATA.liabilities, title);
     MAIN_DATA.assets.getByName = title => getByName(MAIN_DATA.assets, title);
 
-    var pending = MAIN_DATA.liabilities.filter(function(a){
-      return a.status.toLowerCase() === 'pending'
-    }).sort(function(a, b) {
-      return new Date(a.date) - new Date(b.date);
-    });
-    var paid = MAIN_DATA.liabilities.filter(function(a){
-      return a.status.toLowerCase() === 'paid'
-    }).sort(function(a, b) {
-      return new Date(a.date) - new Date(b.date);
-    });
-    var due = MAIN_DATA.liabilities.filter(function(a){
-      return a.status.toLowerCase() === 'due'
-    }).sort(function(a, b) {
-      return new Date(a.date) - new Date(b.date);
-    });
-    var liabilities = [].concat(due, pending, paid);
+    var liabilities = MAIN_DATA.liabilities;
     var assets = MAIN_DATA.assets;
-
-    var pendingTotal = pending
-        .filter(item => !JSON.parse(item.hidden))
-        .reduce((all, one) => all + Number(one.amount), 0);
-    var dueTotal = due
-        .filter(item => !JSON.parse(item.hidden))
-        .reduce((all, one) => all + Number(one.amount), 0);
-    var assetsTotal = assets
-        .filter(item => !JSON.parse(item.hidden))
-        .reduce((all, one) => all + Number(one.amount), 0);
-    var debts = liabilities
-        .filter(item => !JSON.parse(item.hidden))
-        .reduce((all, one) => all + Number(one.amount), 0);
-    var debtsTotal = liabilities
-        .filter(item => !JSON.parse(item.hidden))
-        .reduce((all, one) => all - Number(one.total_owed), 0);
 
     var totals = {
       balance: data.scraped.data.accounts[0].balance,
-      pending: pendingTotal,
-      due: dueTotal,
-      assets: assetsTotal,
-      debts: debts,
-      debtTotal: debtsTotal
+      pending: MAIN_DATA.totals.pendingTotal,
+      due: MAIN_DATA.totals.dueTotal,
+      assets: MAIN_DATA.totals.assetsTotal,
+      debts: MAIN_DATA.totals.debts,
+      debtTotal: MAIN_DATA.totals.debtsTotal
     };
     return {liabilities, assets, totals};
 }
--- server/utilities.js
+++ server/utilities.js
@@ -2,12 +2,12 @@ var fs = require('fs');
 var path = require('path');
 var DEBUG = false;
 
-function getAccountsFileName(){
+function getAccountsFileName() {
   const accountsFile = path.resolve(__dirname, '../accounts.json');
 
   const useDefault = (() => {
     var _useDefault = true;
-    if (fs.existsSync(accountsFile)){
+    if (fs.existsSync(accountsFile)) {
       DEBUG && console.log('--- account file exists');
       try {
         var accountsFileRead = fs.readFileSync(accountsFile).toString();
@@ -22,7 +22,7 @@ function getAccountsFileName(){
           && accountData.hasOwnProperty('balance')
           && accountData.hasOwnProperty('assets')
           && accountData.hasOwnProperty('liabilities')
-        ){
+        ) {
           DEBUG && console.log('--- accounts file does not have proper props');
           _useDefault = false;
         }
@@ -41,6 +41,62 @@ function getAccountsFileName(){
     : accountsFile;
 }
 
+function updateAccounts(accounts) {
+  var u = JSON.parse(JSON.stringify(accounts));
+
+  // AUTO MARK DUE and PAID
+  var today = new Date();
+  var oneWeekAhead = new Date().setDate(today.getDate() + 7);
+  u.liabilities = u.liabilities.map(function (item) {
+    if (item.status.toLowerCase() === 'paid' && new Date(item.date) <= oneWeekAhead) {
+      item.status = "due";
+      console.log(item);
+      if(item.auto){
+        item.status = "pending";
+      }
+    }
+    return item;
+  });
+
+  // SORT LIABILITIES
+  var pending = u.liabilities.filter(function (a) {
+    return a.status.toLowerCase() === 'pending'
+  }).sort(function (a, b) {
+    return new Date(a.date) - new Date(b.date);
+  });
+  var paid = u.liabilities.filter(function (a) {
+    return a.status.toLowerCase() === 'paid'
+  }).sort(function (a, b) {
+    return new Date(a.date) - new Date(b.date);
+  });
+  var due = u.liabilities.filter(function (a) {
+    return a.status.toLowerCase() === 'due'
+  }).sort(function (a, b) {
+    return new Date(a.date) - new Date(b.date);
+  });
+  u.liabilities = [].concat(due, pending, paid);
+
+  u.totals = {};
+  u.totals.pendingTotal = pending
+    .filter(item => !JSON.parse(item.hidden))
+    .reduce((all, one) => all + Number(one.amount), 0);
+  u.totals.dueTotal = due
+    .filter(item => !JSON.parse(item.hidden))
+    .reduce((all, one) => all + Number(one.amount), 0);
+  u.totals.assetsTotal = u.assets
+    .filter(item => !JSON.parse(item.hidden))
+    .reduce((all, one) => all + Number(one.amount), 0);
+  u.totals.debts = u.liabilities
+    .filter(item => !JSON.parse(item.hidden))
+    .reduce((all, one) => all + Number(one.amount), 0);
+  u.totals.debtsTotal = u.liabilities
+    .filter(item => !JSON.parse(item.hidden))
+    .reduce((all, one) => all - Number(one.total_owed), 0);
+
+  return u;
+}
+
 module.exports = {
-  getAccountsFileName
+  getAccountsFileName,
+  updateAccounts
 };
