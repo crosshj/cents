d182544e74370e005b989372e978f0f2262016a1
Cross, Harrison (CORP) <Harrison.Cross@adp.com>
2017-11-01 22:38:07 -0400

    bump cache ver

--- client/sw.js
+++ client/sw.js
@@ -18,7 +18,7 @@ https://serviceworke.rs/
 
 // Update 'version' if you need to refresh the cache
 var staticCacheName = 'static';
-var version = 'v1.0.12::';
+var version = 'v1.0.13::';
 var CACHE = version + staticCacheName;
 var timeout = 1500;
 
