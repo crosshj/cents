f9a67b5e328579c1ca553ba4fa812fe5cbb52387
Cross, Harrison (CORP) <Harrison.Cross@adp.com>
2017-11-21 22:50:06 -0500

    scrape discover (WIP)

--- service/scrapers/puppeteer/scrapers/index.js
+++ service/scrapers/puppeteer/scrapers/index.js
@@ -1,6 +1,7 @@
 function getScrapers(privateInfo){
   return {
-    usaa: require('./usaa.es6')(privateInfo.usaa())
+    usaa: require('./usaa.es6')(privateInfo.usaa()),
+    discover: require('./discover.es6')(privateInfo.discover())
   };
 }
 
