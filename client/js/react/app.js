29957594595fe4920288d050cbcc273aeac3a864
HJ Cross <github@chimpjuice.com>
2017-12-10 01:10:35 -0500

    forgotten from previous commit

--- client/js/react/app.js
+++ client/js/react/app.js
@@ -11,28 +11,36 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { setup as setupStore } from '../redux/store';
+import actions, { init as initActions } from '../redux/actions';
 
 import AppContainer from './components/AppContainer';
+import Popup from './components/Popup';
+
 import {
   getAccounts,
   setupLoginPageListener
 } from './misc';
 
 const store = setupStore(renderApp);
+initActions(store);
 
 function renderApp(props) {
-  const state = Object.assign({}, props, store.getState());
+  const state = store.getState();
   //console.log(state);
   render(
-    React.createElement(AppContainer, state.app, null),
+    React.createElement(AppContainer, Object.assign({}, state.app, {popup: state.popup}), null),
     document.querySelector('#app')
   );
+  // render(
+  //   React.createElement(Popup, state.popup, null),
+  //   document.querySelector('#popup-modal')
+  // );
 }
 
 setupLoginPageListener();
 
 getAccounts((err, res) => {
-  const payload = res || err;
-  payload.error = res.error || false;
-  store.dispatch({type: 'WOW', payload})
+  const payload = res || {};
+  payload.error = err || res.error || false;
+  actions({type: 'GET_ACCOUNTS', payload});
 });
