1627ea8939cefc1a7c96253a470504d95b5e85f8
HJ Cross <github@chimpjuice.com>
2019-08-30 19:33:50 -0400

    Revert "trying to fix zero downtime system"
    This reverts commit be6f34b2948275dd7e169c6dd0bd559dea515895.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # from https://medium.com/@slavakorolev/zero-downtime-deployment-with-docker-d9ef54e48c4
 
-CONTAINER_NAME="cents_"
+CONTAINER_NAME="cents_cents_"
 
 # lets find the first container
 FIRST_NUM=`docker ps | awk '{print $NF}' | grep $CONTAINER_NAME | awk -F  "_" '{print $NF}' | sort | head -1`
@@ -13,7 +13,6 @@ MAX_NUM_OF_CONTAINERS=2
 docker-compose scale cents=$MAX_NUM_OF_CONTAINERS
 
 # waiting for new containers
-echo "Waiting for new containers to boot..."
 sleep 90
 
 # removing old containers
