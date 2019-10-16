24959550382f0a03325c66824855a5e5139cb75b
crosshj <github@chimpjuice.com>
2019-10-16 00:26:18 -0400

    service worker POC, caching

--- serviceWorker/index.js
+++ serviceWorker/index.js
@@ -1,17 +1,26 @@
+(function(){
+
 let newWorker;
-function update(){
-    newWorker.postMessage({ action: 'skipWaiting' });
-}
 
 function showUpdateBar() {
     //let snackbar = document.getElementById('snackbar');
     //snackbar.className = 'show toast-container';
-    var toastHTML = `
-        <span>A new version is available.</span>
-        <button class="btn-flat toast-action"  onclick="update()">UPDATE</button>
-    `;
+    var toastHTML = newWorker
+        ? `
+            <span>A new version is available.</span>
+            <button class="btn-flat toast-action"  onclick="update()">UPDATE</button>
+        `
+        : `
+            <span>Install this application</span>
+            <button class="btn-flat toast-action"  onclick="update()">INSTALL</button>
+        `;
+
     //TODO: on dismiss remember until next update
-    M.toast({html:toastHTML, displayLength: Number.MAX_SAFE_INTEGER})
+    M.toast({ html: toastHTML, displayLength: Number.MAX_SAFE_INTEGER })
+}
+
+function hideUpdateBar(){
+    M.Toast.dismissAll();
 }
 
 function updateStatus(status) {
@@ -19,46 +28,70 @@ function updateStatus(status) {
     swStatus.innerHTML = status;
 }
 
-if ('serviceWorker' in navigator) {
-    if(navigator.serviceWorker.controller) {
-        console.log('-- alrready a sw registered');
-        updateStatus('a service worker is already registered');
-        console.log(navigator.serviceWorker.controller);
-    } else {
-        updateStatus('a service worker is NOT registered');
+function registerSW() {
+    if(!navigator.onLine){
+        console.log('-- offline, will not register service worker');
+        return;
     }
-    navigator.serviceWorker
-        .register('service-worker.js')
-        .then(reg => {
-            console.log({ reg });
-            if(reg.waiting){
-                newWorker = reg.waiting;
-                showUpdateBar();
-            }
-            reg.addEventListener('updatefound', () => {
-                // A wild service worker has appeared in reg.installing!
-                newWorker = reg.installing;
-                newWorker.addEventListener('statechange', () => {
-                    console.log('service worker state:');
-                    console.log(newWorker.state);
-                    // Has network.state changed?
-                    switch (newWorker.state) {
-                        case 'installed':
-                            if (navigator.serviceWorker.controller) {
-                                // new update available
-                                showUpdateBar();
-                            }
-                            // No update available
-                            break;
-                    }
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('service-worker.js', { scope: './' })
+            .then(reg => {
+                console.log({ reg });
+                if (reg.waiting) {
+                    newWorker = reg.waiting;
+                    showUpdateBar();
+                }
+                reg.addEventListener('updatefound', () => {
+                    // A wild service worker has appeared in reg.installing!
+                    newWorker = reg.installing;
+                    newWorker.addEventListener('statechange', () => {
+                        console.log('service worker state:');
+                        console.log(newWorker.state);
+                        // Has network.state changed?
+                        switch (newWorker.state) {
+                            case 'activated':
+                                updateStatus('a service worker is already registered');
+                                hideUpdateBar();
+                                break;
+                        }
+                    });
                 });
             });
-        });
-
-    let refreshing;
-    navigator.serviceWorker.addEventListener('controllerchange', function () {
-        if (refreshing) return;
-        window.location.reload();
-        refreshing = true;
-    });
+        return;
+    }
 }
+
+function update() {
+    if(!newWorker){
+        registerSW();
+        return;
+    }
+    newWorker.postMessage({ action: 'skipWaiting' });
+}
+window.update = update;
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    if ('serviceWorker' in navigator) {
+        const currentSW = navigator.serviceWorker.controller;
+        if (currentSW && currentSW.scriptURL.includes("sw/service-worker.js")) {
+            console.log('-- alrready a sw registered');
+            updateStatus('a service worker is already registered');
+            console.log(currentSW);
+            registerSW(); //for updates...
+        } else {
+            updateStatus('a service worker is NOT registered');
+            showUpdateBar();
+        }
+
+        let refreshing;
+        navigator.serviceWorker.addEventListener('controllerchange', function () {
+            if (refreshing) return;
+            window.location.reload();
+            refreshing = true;
+        });
+    }
+
+}); // DOMContentLoaded
+}()); // closure
--- serviceWorker/service-worker.js
+++ serviceWorker/service-worker.js
@@ -1,10 +1,26 @@
-var cacheName = 'secondVersion';
+var cacheName = 'secondVersion-foo';
+
+const filesToCache = [
+  './',
+  'index.css',
+  'index.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
-      ]))
+      .then(cache => cache.addAll(filesToCache))
+
+      // .then(cache => cache.addAll(filesToCache
+      //     .map(function(urlToPrefetch) {
+      //       return new Request(urlToPrefetch, { mode: 'no-cors' });
+      //     })
+      //   )
+      // )
   );
 });
 
