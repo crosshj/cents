c35640a47e08ac6f1c1f56446350cde43af80d62
crosshj <github@chimpjuice.com>
2019-10-17 01:35:46 -0400

    Update service-worker.js

--- serviceWorker/service-worker.js
+++ serviceWorker/service-worker.js
@@ -14,7 +14,6 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => cache.addAll(filesToCache))
-
       // .then(cache => cache.addAll(filesToCache
       //     .map(function(urlToPrefetch) {
       //       return new Request(urlToPrefetch, { mode: 'no-cors' });
